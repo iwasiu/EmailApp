7e192e71d6c20ad778b12e3bca2483b86d259c79
iwasiu <iwasiu460@gmail.com>
2021-02-28 18:40:20 +0100

    Create EmailMain class

---
+++ EmailApp/src/EmailApplication/EmailMain.java
@@ -0,0 +1,11 @@
+package EmailApplication;
+
+public class EmailMain {
+
+	public static void main(String[] args) {
+		// create an object of Email class
+		Email myEmail = new Email("Wasiu", "IDowu");
+	
+	}
+
+}
